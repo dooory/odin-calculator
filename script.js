26c1ca1ae10dd972e7d99426537fb545514393f8
doory <78720364+dooory@users.noreply.github.com>
2025-07-02 10:53:54 +1200

    feat: create functions for each basic operator

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("test");
+function addition(x, y) {
+	return x + y;
+}
+
+function subtraction(x, y) {
+	return x - y;
+}
+
+function division(x, y) {
+	return x / y;
+}
+
+function multiplication(x, y) {
+	return x * y;
+}
