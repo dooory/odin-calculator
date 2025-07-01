568494d3d264f7c0ee4b8cc97ca7a086d8d2d73a
doory <78720364+dooory@users.noreply.github.com>
2025-07-02 11:47:21 +1200

    feat: create operate function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function addition(x, y) {
 	return x + y;
 }
@@ -13,3 +14,43 @@ function division(x, y) {
 function multiplication(x, y) {
 	return x * y;
 }
+
+// Associates different synonyms for operators to that operators function
+let operatorAliases = {
+	add : [addition, "+", "addition", "add"],
+	subtract : [subtraction, "-", "substraction", "subtract"],
+	multiply : [multiplication, "*", "multiplication", "multiply"],
+	divide : [division, "/", "substraction", "subtract"],
+}
+
+// Translates an operator in the form of a string to its function
+function getOperatorsFunction(operator) {
+	operator = operator.toLowerCase();
+
+	for (const index in operatorAliases) {
+		if (operatorAliases.hasOwnProperty(index)) {
+			let operatorArray = operatorAliases[index];
+
+			if (operatorArray.includes(operator)) {
+				return operatorArray[0];
+			}
+		}
+	}
+}
+
+function operate(operator, x, y) {
+	// sets y to x if y is null
+	y = y || x;
+
+	let operatorFunction = getOperatorsFunction(operator);
+
+	let sum = operatorFunction(x, y);
+
+	return sum;
+}
+
+// let numberX = 1;
+// let numberY = 2;
+// let operator = "Add"
+//
+// const sum = operate(operator, numberX, numberY);
