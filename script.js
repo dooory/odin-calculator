a5738e8c293cba9905e4e0179561588ff03866d9
doory <78720364+dooory@users.noreply.github.com>
2025-07-02 15:07:04 +1200

    feat: add functionality to clear and equals buttons

--- script.js
+++ script.js
@@ -18,9 +18,9 @@ function multiplication(x, y) {
 // Associates different synonyms for operators to that operators function
 let operatorAliases = {
 	add : [addition, "+", "addition", "add"],
-	subtract : [subtraction, "-", "substraction", "subtract"],
-	multiply : [multiplication, "*", "multiplication", "multiply"],
-	divide : [division, "/", "substraction", "subtract"],
+	subtract : [subtraction, "-", "–", "substraction", "subtract"],
+	multiply : [multiplication, "*", "x", "multiplication", "multiply"],
+	divide : [division, "/", "÷", "division", "divide"],
 }
 
 // Translates an operator in the form of a string to its function
@@ -39,8 +39,15 @@ function getOperatorsFunction(operator) {
 }
 
 function operate(operator, x, y) {
+	if (operator === "") {
+		return x || 0;
+	}
+
 	// sets y to x if y is null
 	y = y || x;
+
+	x = parseInt(x);
+	y = parseInt(y);
 
 	let operatorFunction = getOperatorsFunction(operator);
 
@@ -49,9 +56,10 @@ function operate(operator, x, y) {
 	return sum;
 }
 
-let numberX;
-let numberY;
-let operator = ""
+let numberX = "";
+let numberY = "";
+let currentOperator = "";
+let evaluated = false;
 
 let currentInput = "";
 let inputDisplay = document.querySelector("#input");
@@ -60,7 +68,24 @@ let numberButtons = document.querySelectorAll(".number");
 
 numberButtons.forEach(button => {
 	button.addEventListener("click", function() {
-		currentInput += button.innerText;
+		let number = button.innerText;
+
+		if (currentOperator === '') {
+			if (numberX == "0") {
+				numberX = ""
+			}
+
+			numberX += number;
+			currentInput = numberX;
+		} else if (currentOperator !== '' && numberX !== ''){
+			if (numberY == "0") {
+				numberY = ""
+			}
+
+			numberY += number;
+			currentInput = numberX + currentOperator + numberY;
+		}
+
 		inputDisplay.innerText = currentInput;	
 	})
 });
@@ -70,8 +95,36 @@ let mainOperatorButtons = mainOperatorsContainer.querySelectorAll(".operator");
 
 mainOperatorButtons.forEach(button => {
 	button.addEventListener("click", function() {
-		currentInput += button.innerText;
-		inputDisplay.innerText = currentInput;	
+		let operator = button.innerText;
+
+		if (numberX !== '') {
+			currentOperator = operator;
+			currentInput = numberX + currentOperator + numberY;
+			inputDisplay.innerText = currentInput;	
+		}
 	})
 });
 
+let clearButton = document.querySelector("#clear");
+clearButton.addEventListener("click", function () {
+	numberX = "";
+	numberY = "";
+	currentOperator = "";
+
+	currentInput = '';
+	inputDisplay.innerText = '0';
+})
+
+let equalsButton = document.querySelector("#equals");
+
+equalsButton.addEventListener("click", function () {
+	let sum = operate(currentOperator, numberX, numberY);
+
+	currentOperator = "";
+	numberY = "";
+
+	currentInput = sum;
+	inputDisplay.innerText = sum;
+
+	numberX = sum;
+})
